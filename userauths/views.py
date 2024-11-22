37758499a8d6ce3798b8239a1a47daefc60f4b3b
@ngelBruno1217 <20030216@itcelaya.edu.mx>
2024-11-22 01:59:48 -0600

    Autenticacion y formularios de usuarios

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from userauths.forms import UserRegisterForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == "POST":
+        print("Usuario registrado exitosamente")
+    else:
+        print("No se ha podido registrar el usuario")
+
+    form = UserRegisterForm()
+
+    context = {
+        'form' : form,
+    }
+    return render(request, "userauths/sign-up.html", context)
