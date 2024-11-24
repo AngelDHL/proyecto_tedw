acb79917e6056b676ff8cbee662757ab725be234
@ngelBruno1217 <20030216@itcelaya.edu.mx>
2024-11-24 08:32:49 -0600

    Reviews finalizado

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -1,8 +1,14 @@
 console.log("Working fine...")
 
+const monthNames = ["Jan","Feb","Mar","April","May","June",
+    "July","Aug","Sept","Oct","Nov","Dec"
+];
 
 $("#commentForm").submit(function(e){
     e.preventDefault();
+
+    let dt = new Date();
+    let time = dt.getDate() + " " + monthNames[dt.getUTCMonth()] + ", " + dt.getFullYear()
 
     $.ajax({
         data: $(this).serialize(),
@@ -18,7 +24,38 @@ $("#commentForm").submit(function(e){
 
             if(res.bool == true){
                 $("#review-res").html("Review added succesfully.")
+                $(".hide-comment-form").hide()
+                $(".add-review").hide()
+
+                let _html = '<div class="single-comment justify-content-between d-flex mb-30">'
+                    _html +='<div class="user justify-content-between d-flex">'
+                    _html +='<div class="thumb text-center">'
+                    _html +='<img src="https://w7.pngwing.com/pngs/177/551/png-transparent-user-interface-design-computer-icons-default-stephen-salazar-graphy-user-interface-design-computer-wallpaper-sphere-thumbnail.png" alt="" />'
+                    _html +='<a href="#" class="font-heading text-brand">'+ res.context.user +'</a>'
+                    _html +='</div>'
+
+                    _html +='<div class="desc">'
+                    _html +='<div class="d-flex justify-content-between mb-10">'
+                    _html +='<div class="d-flex align-items-center">'
+                    _html +='<span class="font-xs text-muted">'+ time +'</span>'
+                    _html +='</div>'
+
+                    for(var i = 1; i <= res.context.rating; i++){
+                        _html += '<i class="fas fa-star text-warning"></i>';
+                    }
+
+                    _html +='</div>'
+                    _html +='<p class="mb-10">'+ res.context.review +'</p>'
+
+                    _html +='</div>'
+                    _html +='</div>'
+                    _html +='</div>'
+
+                    $(".comment-list").prepend(_html)
             }
+
+            
+
         }
     })
 })
