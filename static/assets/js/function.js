21fd8d71b66d411f20b8b8f8b777e4e97db9e539
@ngelBruno1217 <20030216@itcelaya.edu.mx>
2024-11-24 14:04:55 -0600

    Filtro de productos por categoria y vendedor 1

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -59,3 +59,26 @@ $("#commentForm").submit(function(e){
         }
     })
 })
+
+
+
+$(document).ready(function (){
+    $(".filter-checkbox").on("click", function(){
+        console.log("A checkbox have been clicked");
+        
+        let filter_object = {}
+
+        $(".filter-checkbox").each(function(){
+            let filter_value = $(this).val()
+            let filter_key = $(this).data("filter")
+
+            // console.log("Filter values is:", filter_value);
+            // console.log("Filter values is:", filter_key);
+
+            filter_object[filter_key] = Array.from(document.querySelectorAll('input[data-filter=' + filter_key + ']:checked')).map(function(element){
+                return element.value
+            })
+        })
+        console.log("Filter Object is: ", filter_object);
+    })
+})
