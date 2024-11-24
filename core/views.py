53d728eb6e54e919bfdc32974c738c9d082139b9
@ngelBruno1217 <20030216@itcelaya.edu.mx>
2024-11-23 20:30:55 -0600

    Vista categoria-producto implemendata

--- core/views.py
+++ core/views.py
@@ -21,7 +21,7 @@ def product_list_view(request):
         "products":products,
     }
 
-    return render(request, 'core/product-list.html', context)
+    return render(request, 'core/product-list.html', context) 
 
 def category_list_view(request):
     categories = Category.objects.all().annotate(product_count=Count("title"))
@@ -31,3 +31,15 @@ def category_list_view(request):
     }
 
     return render(request, 'core/category-list.html', context)
+
+def category_product_list_view(request,cid):
+    category = Category.objects.get(cid=cid)
+    products = Product.objects.filter(product_status="published", category=category)
+
+    context = {
+        "category":category,
+        "products":products,
+    }
+
+    return render(request, "core/category-product-list.html", context)
+
