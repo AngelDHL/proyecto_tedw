170b0e70050b8c31c0c9a4e006de0acb2c6d0773
@ngelBruno1217 <20030216@itcelaya.edu.mx>
2024-11-27 01:26:24 -0600

    Add products to cart

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -131,35 +131,78 @@ $(document).ready(function (){
     });
 })
 
-$("#add-to-cart-btn").on("click", function(){
-    let quantity = $("#product-quantity").val()
-    let product_title = $(".product-title").val()
-    let product_id = $(".product-id").val()
-    let product_price = $("#prueba2").text()
+$(".add-to-cart-btn").on("click", function(){
+
     let this_val = $(this)
+    let index = this_val.attr("data-index")
+
+    let quantity = $(".product-quantity-" + index).val()
+    let product_title = $(".product-title-" + index).val()
+    let product_id = $(".product-id-" + index).val()
+    let product_price = $(".prueba2-" + index).text()
+    let product_pid = $(".product-pid-" + index).val()
+    let product_image = $(".product-image-" + index).val()
 
     console.log("Quantity:", quantity);
-    console.log("Id:", product_id);
     console.log("Title:", product_title);
     console.log("Price:", product_price);
+    console.log("Id:", product_id);
+    console.log("PId:", product_pid);
+    console.log("Image:", product_image);
+    console.log("Index:", index);
     console.log("Currente Element:", this_val);
 
-    $.ajax({
-        url: '/add-to-cart',
-        data: {
-            'id': product_id,
-            'qty': quantity,
-            'title': product_title,
-            'price': product_price,
-        },
-        dataType: 'json',
-        beforeSend: function(){
-            console.log("Adding Product to cart...");
-        },
-        success: function(res){
-            this_val.html("Item added to cart");
-            console.log("Added Product to Cart!");
-            $(".cart-items-count").text(response.totalcartitems)
-        }
-    })
+     $.ajax({
+         url: '/add-to-cart',
+         data: {
+             'id': product_id,
+             'pid': product_pid,
+             'image': product_image,
+             'qty': quantity,
+             'title': product_title,
+             'price': product_price,
+         },
+         dataType: 'json',
+         beforeSend: function(){
+             console.log("Adding Product to cart...");
+         },
+         success: function(response){
+             this_val.html("✔");
+             console.log("Added Product to Cart!");
+             $(".cart-items-count").text(response.totalcartitems)
+         }
+     })
 })
+
+// $(".add-to-cart-btn").on("click", function(){
+//     let quantity = $("#product-quantity").val()
+//     let product_title = $(".product-title").val()
+//     let product_id = $(".product-id").val()
+//     let product_price = $("#prueba2").text()
+//     let this_val = $(this)
+
+//     console.log("Quantity:", quantity);
+//     console.log("Id:", product_id);
+//     console.log("Title:", product_title);
+//     console.log("Price:", product_price);
+//     console.log("Currente Element:", this_val);
+
+//     $.ajax({
+//         url: '/add-to-cart',
+//         data: {
+//             'id': product_id,
+//             'qty': quantity,
+//             'title': product_title,
+//             'price': product_price,
+//         },
+//         dataType: 'json',
+//         beforeSend: function(){
+//             console.log("Adding Product to cart...");
+//         },
+//         success: function(res){
+//             this_val.html("Item added to cart");
+//             console.log("Added Product to Cart!");
+//             $(".cart-items-count").text(response.totalcartitems)
+//         }
+//     })
+// })
