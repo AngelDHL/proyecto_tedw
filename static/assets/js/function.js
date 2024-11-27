eb5b901086679002e294da4e3c71454bb5677fcf
@ngelBruno1217 <20030216@itcelaya.edu.mx>
2024-11-26 23:49:01 -0600

    Add to cart 2

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -159,6 +159,7 @@ $("#add-to-cart-btn").on("click", function(){
         success: function(res){
             this_val.html("Item added to cart");
             console.log("Added Product to Cart!");
+            $(".cart-items-count").text(response.totalcartitems)
         }
     })
 })
